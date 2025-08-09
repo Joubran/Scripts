9b5c5b71c230237dfd9cbab601c30d0be6745eae
Jay <96941158+Joubran@users.noreply.github.com>
2025-08-09 13:42:40 +1000

    feat: event-driven upgrade availability

--- Coins & upgrades/Upgrades/UpgradeZone.cs
+++ Coins & upgrades/Upgrades/UpgradeZone.cs
@@ -5,19 +5,40 @@ public class UpgradeZone : MonoBehaviour {
     public int upgradeIndex;
     private UpgradeManager upgradeManager;
     public GameObject popupPrefab;
+    private UpgradeData data;
 
     void Awake() {
         upgradeManager = FindObjectOfType<UpgradeManager>();
     }
 
-    void Update() {
-        var data = upgradeManager.upgrades[upgradeIndex];
-        // only show if not obtained and player has enough coins
-        if (!data.isObtained && CoinManager.Instance.HasCoins(data.upgradeCost))
-        {
+    void OnEnable() {
+        data = upgradeManager.upgrades[upgradeIndex];
+        CoinManager.Instance.CurrencyChanged += OnCurrencyOrPriceChanged;
+        data.CostChanged += OnCurrencyOrPriceChanged;
+        CheckAffordable();
+    }
+
+    void OnDisable() {
+        if (CoinManager.Instance != null)
+            CoinManager.Instance.CurrencyChanged -= OnCurrencyOrPriceChanged;
+        if (data != null)
+            data.CostChanged -= OnCurrencyOrPriceChanged;
+    }
+
+    private void OnCurrencyOrPriceChanged() {
+        CheckAffordable();
+    }
+
+    private void OnCurrencyOrPriceChanged(CoinAmount _) {
+        CheckAffordable();
+    }
+
+    private void CheckAffordable() {
+        if (!data.isObtained && CoinManager.Instance.HasCoins(data.upgradeCost)) {
             var popup = Instantiate(popupPrefab, transform.position + Vector3.up * 2f, Quaternion.identity);
             popup.GetComponent<UpgradePopup>().Initialize(upgradeIndex, data, upgradeManager);
-            GetComponent<UpgradeZone>().enabled = false;
+            enabled = false;
         }
     }
 }
+
